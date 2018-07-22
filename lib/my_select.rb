6c1dc05bf075a5379d67c8ee4450d7f959064d44
Justin Wang <justinw827@aol.com>
2018-07-22 21:07:40 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
 def my_select(collection)
- 
+  retVal = []
+  collection.each_with_index do |e, i|
+    if yield(e)
+      retVal.push(collection[i])
+      i += 1
+    end
+  end
+  return retVal
 end
